31b330f1926ff4150edf89011ed2c7297795b745
umarkhattak <ukhattak100@gmail.com>
2021-10-11 11:18:36 -0400

    test stuff

--- StudentManager/src/studentmanager/test/InternationalTest.java
+++ StudentManager/src/studentmanager/test/InternationalTest.java
@@ -1,4 +1,16 @@
 package studentmanager.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import studentmanager.student.International;
+
+import java.util.Calendar;
+
+/**
+ * JUnit test class for International.tuitionDue method
+ *
+ * @author Noor, Umar
+ */
+
 public class InternationalTest {
 }
--- StudentManager/src/studentmanager/test/RosterTest.java
+++ StudentManager/src/studentmanager/test/RosterTest.java
@@ -1,4 +1,15 @@
 package studentmanager.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import studentmanager.Roster;
+
+import java.util.Calendar;
+
+/**
+ * JUnit test class for Roster.add and Roster.remove method
+ *
+ * @author Noor, Umar
+ */
 public class RosterTest {
 }
